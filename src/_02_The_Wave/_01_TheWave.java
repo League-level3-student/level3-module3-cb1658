fc2a068da5145c751fa85132bc535da92ed17bf0
smellstink <54760319+OceanNut420@users.noreply.github.com>
2023-01-20 16:42:51 -0800

    Level 3 Module 03 02 COMPLETE

--- src/_02_The_Wave/_01_TheWave.java
+++ src/_02_The_Wave/_01_TheWave.java
@@ -24,23 +24,126 @@ public class _01_TheWave {
     	    	
     	ArrayList<String> arrlist = new ArrayList<String>();
     	
+    	ArrayList<Integer> indexList = new ArrayList<Integer>();
     	
-    	for(int i = 0; i< str.length(); i++) {
+    	for(int i = 0; i < str.length(); i++) {
+    		if(!Character.isWhitespace(str.charAt(i))) {
+    			indexList.add(i);
+    		}
+    	}
+    	
+    	
+    	for(int k = 0; k < indexList.size(); k++) {
     		StringBuilder bob = new StringBuilder();
-    		int memory = i;
-    		
-    		for(int k = 0; k< str.length(); k++) {
-    			char a = str.charAt(k);
-    			if(!Character.isWhitespace(a) && k == i) {
-    				bob.append(Character.toUpperCase(a));
+    		for(int a = 0; a < str.length(); a++) {
+    			if(a == indexList.get(k)) {
+    				bob.append(Character.toUpperCase(str.charAt(a)));
     			}
     			else {
-    				bob.append(a);
+    				bob.append(str.charAt(a));
     			}
     		}
     		arrlist.add(bob.toString());
     	}
-        
+   
         return arrlist;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    	for(int i = 0; i< str.length(); i++) {
+//    		StringBuilder bob = new StringBuilder();
+//    		int memory = i;
+//    		
+//    		for(int k = 0; k< str.length(); k++) {
+//    			char a = str.charAt(k);
+//    			if(!Character.isWhitespace(a) && k == i) {
+//    				bob.append(Character.toUpperCase(a));
+//    			}
+//    			else if(Character.isWhitespace(a) && k == i){
+//    				memory = k;
+////    				 	while(Character.isWhitespace(a)) {
+////    				 		if(Character.isWhitespace(str.charAt(memory))) {
+////    				 			bob.append(str.charAt(memory));
+////    				 			if(memory == str.length()-1) {
+////    				 				break;
+////    				 			}
+////    				 			memory += 1;
+////    				 			a = str.charAt(memory);
+////    				 		}
+////
+////    				 		else {
+////    				 			bob.append(Character.toUpperCase(str.charAt(memory)));
+////    				 		}
+////    				 	}
+//    				 	
+//    			}
+//    			else {
+//    				bob.append(a);
+//    			}
+//
+//    		}
+//    		arrlist.add(bob.toString());
+//    	}
+//     
