b0da7bb8e94f391a4c914e427fd0cf015dc912bf
league <league@imac-16.attlocal.net>
2023-01-27 15:59:01 -0800

    Level 3 Module 3 COMPLETE

--- src/_03_Intro_To_Regular_Expressions/_01_RegularExpressionsIntro.java
+++ src/_03_Intro_To_Regular_Expressions/_01_RegularExpressionsIntro.java
@@ -17,6 +17,7 @@ public class _01_RegularExpressionsIntro {
      * Code method below! 
      */
     String emailRemover(String message) {
-        return null;
+    	String regex = "[@][\\w]+[.][\\w]+";
+        return message.replaceAll(regex, "@mail.com");
     }
 }
